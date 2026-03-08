4d9aebc054fd0de22093fc0235b7834fa3081257
rejep <rejep.hekim@gmail.com>
2026-03-08 16:51:58 -0500

    TouchSensor Practice

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TouchSensorPractice.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.TestBench;
+
+@TeleOp
+public class TouchSensorPractice extends OpMode {
+    TestBench bench = new TestBench();
+    String isPressed = "";
+
+    @Override
+    public void init() {
+        bench.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Touch Sensor State", bench.getTouchSensor());
+
+        if (bench.isReleased()) {
+            isPressed = "Not Pressed";
+        }
+        else {
+            isPressed = "Pressed";
+        }
+        telemetry.addData("Is touch sensor released", isPressed);
+    }
+}
